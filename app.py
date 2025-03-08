9d4c42eebb67288c7d5de994333d329fef877ebb
Mia <138697391+miayanceyy@users.noreply.github.com>
2025-03-07 23:16:37 -0500

    Update app.py
    Update to use the index template

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    lat = .1234
+    lon = .5678
+    return render_template('index.html', latitude=lat, longitude=lon)
+
+if __name__ == '__main__':
+    app.run(debug=True)
